83d2a1eaec419379850934f7032064263d0c5392
kakar <kakar@LAPTOP-L8K491A0>
2023-03-27 05:45:28 +0530

    First commit

--- src/GqtCodes/Program1.java
+++ src/GqtCodes/Program1.java
@@ -3,7 +3,7 @@ package GqtCodes;
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
         System.out.println("Hello World");
         }
 
